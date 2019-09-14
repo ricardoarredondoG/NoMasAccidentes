4a247690369c54ed345a824a65d7b096ff045f56
Ricardo <ricard.arredondo@gmail.com>
2019-09-14 12:07:17 -0300

    Modificacion Login

--- NoMasAccidentes/Controllers/HomeController.cs
+++ NoMasAccidentes/Controllers/HomeController.cs
@@ -1,42 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
 using System.Web.Mvc;
-using NoMasAccidentes.Models;
 
 namespace NoMasAccidentes.Controllers
 {
     public class HomeController : Controller
     {
-   
-
-        public ActionResult Login(String message ="")
+        [Authorize]
+        public ActionResult Index()
         {
-            ViewBag.Message = message;
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Ingresar(String username, String password, int tipo_usuario)
-        {
-            EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
-
-            var user = bd.PERSONAL.FirstOrDefault(e => e.USERNAME_PERSO == username && e.PASSWORD_PERSO == password);
-
-            if (user != null)
-            {
-                return RedirectToAction("Login", new { message = " " });
-            }
-            else
-            {
-                return RedirectToAction("Login", new { message = "*Los Datos Ingresados no son Validos " });
-            }
-
-
-            
-        }
-
-
+        
     }
 }
---
+++ NoMasAccidentes/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using NoMasAccidentes.Models;
+
+namespace NoMasAccidentes.Controllers
+{
+    public class LoginController : Controller
+    {
+   
+
+        public ActionResult Index(String message ="")
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingresar(String username, String password, int tipo_usuario)
+        {
+            EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
+
+            var user = bd.PERSONAL.FirstOrDefault(e => e.USERNAME_PERSO == username && e.PASSWORD_PERSO == password);
+
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.USERNAME_PERSO, true);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", new { message = "*Los Datos Ingresados no son Validos " });
+            }
+
+
+            
+        }
+
+
+    }
+}
