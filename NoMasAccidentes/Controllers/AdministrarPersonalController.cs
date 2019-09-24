ad6a510b39bbd125ee69bb479a559e72b8382f31
Ricardo <ricard.arredondo@gmail.com>
2019-09-24 19:38:03 -0300

    Gestionar Personal, Listar, Filtros de busquedas y paginacion terminados

--- NoMasAccidentes/Controllers/AdministrarPersonalController.cs
+++ NoMasAccidentes/Controllers/AdministrarPersonalController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NoMasAccidentes.Models;
+using NoMasAccidentes.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,52 @@ namespace NoMasAccidentes.Controllers
     public class AdministrarPersonalController : Controller
     {
         // GET: AdministrarPersonal
-        public ActionResult Index()
+        public ActionResult Index(int pagina= 1, string nombre= "", string apellidoP="", string usuario="", string correo="")
         {
-            return View();
+            var cantidadRegistrosPorPagina = 4;
+            EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
+            var personal = bd.PERSONAL.ToList();
+           
+            //Busqueda por Nombre
+            if (nombre != "")
+            {
+                personal = personal.FindAll(x => x.NOMBRE_PERSO.ToLower().Contains(nombre.ToLower()));
+            }
+            //Busqueda por Apellido Paterno
+
+            if (apellidoP != "")
+            {
+                personal = personal.FindAll(x => x.APELLIDOP_PERSO.ToLower().Contains(apellidoP.ToLower()));
+            }
+
+            //Busqueda por Usuario
+
+            if (usuario != "")
+            {
+                personal = personal.FindAll(x => x.USERNAME_PERSO.ToLower().Contains(usuario.ToLower()));
+            }
+
+            //Busqueda por Correo Electronico
+
+            if (correo != "")
+            {
+                personal = personal.FindAll(x => x.CORREO_PERSO.ToLower().Contains(correo.ToLower()));
+            }
+
+
+            var totalRegistros = personal.Count();
+            personal = personal.OrderBy(x => x.ID_PERSONAL).Skip((pagina - 1) * cantidadRegistrosPorPagina).Take(cantidadRegistrosPorPagina).ToList();
+
+
+            var modelo = new IndexViewModel();
+
+            modelo.personal = personal;
+            modelo.PaginaActual = pagina;
+            modelo.TotalDeRegistros = totalRegistros;
+            modelo.RegistrosPorPagina = cantidadRegistrosPorPagina;
+
+
+            return View(modelo);
         }
     }
 }
