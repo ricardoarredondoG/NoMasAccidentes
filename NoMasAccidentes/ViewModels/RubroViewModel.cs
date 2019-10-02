b86b6c794989e7594c2581579bdace46fdb85664
Michal <mich.orellana@alumnos.duoc.cl>
2019-10-01 21:54:47 -0300

    Comienzo de Administrar Rubro - Listar

---
+++ NoMasAccidentes/ViewModels/RubroViewModel.cs
@@ -0,0 +1,21 @@
+﻿using NoMasAccidentes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoMasAccidentes.ViewModels
+{
+    public class RubroViewModel : BaseModelo
+    {
+        //public List<Models.RUBRO> rubro { get; set; }
+
+
+        public String nombre_rubro { get; set; }
+        public String desc_rubro { get; set; }
+
+
+    }
+
+    
+}
