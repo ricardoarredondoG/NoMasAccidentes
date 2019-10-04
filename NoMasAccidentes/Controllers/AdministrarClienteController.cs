851532a69bb6906a63745c9893e6f9b911342518
Ro <robertopizarro199@gmail.com>
2019-10-04 14:49:27 -0600

    Eliminar.

--- NoMasAccidentes/Controllers/AdministrarClienteController.cs
+++ NoMasAccidentes/Controllers/AdministrarClienteController.cs
@@ -42,6 +42,7 @@ namespace NoMasAccidentes.Controllers
             var modelo = new IndexViewModel();
             modelo.cliente = cliente;
             modelo.PaginaActual = pagina;
+            modelo.RegistrosPorPagina = cantidadRegistroPorPagina;
             modelo.TotalDeRegistros = totalRegistros;
 
 
@@ -52,59 +53,126 @@ namespace NoMasAccidentes.Controllers
         [Authorize]
         public JsonResult CrearCliente(ClienteViewModel cliente)
         {
-            EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
-            NoMasAccidentes.Models.CLIENTE clienteR = new CLIENTE();
-
-
-            clienteR.RUT_CLIENTE = cliente.rut_cliente;
-            clienteR.NOMBRE_CLIENTE = cliente.nombre_cliente;
-            clienteR.APELLIDO_CLIENTE = cliente.apellido_cliente;
-            clienteR.TELEFONO_CLIENTE = cliente.telefono_cliente;
-            clienteR.DIREC_CLIENTE = cliente.direc_cliente;
-            clienteR.CORREO_CLIENTE = cliente.correo_cliente;
-            clienteR.RUBRO_ID_RUBRO = cliente.rubro_id_rubro;
-            clienteR.ACTIVO_CLIENTE = "S";
-            //Espacios en blanco
-
-            var nombre = cliente.nombre_cliente.Replace(" ", "");
-            var apellido = cliente.apellido_cliente.Replace(" ", "");
-            var username = "";
-            var user_encontrado = false;
-            var cantidad_caracter = 3;
-
-
-            //Buscar cliente
-
-            while (!user_encontrado)
+            //Validaciones.
+            var resul = new baseRespuesta();
+            resul.ok = true;
+            if (cliente.rut_cliente == null)
             {
-                username = (nombre.Substring(0, cantidad_caracter) + "." + apellido).ToLower();
-
-                if (bd.CLIENTE.ToList().FindAll(x => x.USERNAME_CLIENTE.Contains(username)).Count() == 0)
-                {
-                    user_encontrado = true;
-
-                }
-                else
-                {
-                    cantidad_caracter++;
-                }
-
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese rut</br>";
+                resul.ok = false;
+            }
+            if (cliente.nombre_cliente == null)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese nombre</br>";
+                resul.ok = false;
+            }
+            if (cliente.apellido_cliente == null)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese apellido</br>";
+                resul.ok = false;
+            }
+            if (cliente.telefono_cliente == null)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese telefono</br>";
+                resul.ok = false;
+            }
+            if (cliente.direc_cliente == null)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese direccion</br>";
+                resul.ok = false;
+            }
+            if (cliente.correo_cliente == null)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Ingrese correo</br>";
+                resul.ok = false;
+            }
+            if (cliente.rubro_id_rubro !=1 && cliente.rubro_id_rubro !=2)
+            {
+                resul.mensaje = resul.mensaje + "<i class ='zmdi zmdi-alert-circle zmdi-hc-fw'></i>Seleccione rubro</br>";
+                resul.ok = false;
             }
 
-            clienteR.USERNAME_CLIENTE = username;
-
-            var guid = Guid.NewGuid();
-            var justNumbers = new String(guid.ToString().Where(Char.IsDigit).ToArray());
-            var password = int.Parse(justNumbers.Substring(4, 4));
-            clienteR.PASSWORD_CLIENTE = password.ToString();
+            if (resul.ok == true)
+            {
+                EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
+                NoMasAccidentes.Models.CLIENTE clienteR = new CLIENTE();
 
 
-            bd.CLIENTE.Add(clienteR);
+                clienteR.RUT_CLIENTE = cliente.rut_cliente;
+                clienteR.NOMBRE_CLIENTE = cliente.nombre_cliente;
+                clienteR.APELLIDO_CLIENTE = cliente.apellido_cliente;
+                clienteR.TELEFONO_CLIENTE = cliente.telefono_cliente;
+                clienteR.DIREC_CLIENTE = cliente.direc_cliente;
+                clienteR.CORREO_CLIENTE = cliente.correo_cliente;
+                clienteR.RUBRO_ID_RUBRO = cliente.rubro_id_rubro;
+                //Espacios en blanco
+
+                var nombre = cliente.nombre_cliente.Replace(" ", "");
+                var apellido = cliente.apellido_cliente.Replace(" ", "");
+                var username = "";
+                var user_encontrado = false;
+                var cantidad_caracter = 3;
+
+
+                //Buscar cliente
+
+                while (!user_encontrado)
+                {
+                    username = (nombre.Substring(0, cantidad_caracter) + "." + apellido).ToLower();
+
+                    if (bd.CLIENTE.ToList().FindAll(x => x.USERNAME_CLIENTE.Contains(username)).Count() == 0)
+                    {
+                        user_encontrado = true;
+
+                    }
+                    else
+                    {
+                        cantidad_caracter++;
+                    }
+
+                }
+
+                clienteR.USERNAME_CLIENTE = username;
+                clienteR.ACTIVO_CLIENTE = "S";
+
+
+                var guid = Guid.NewGuid();
+                var justNumbers = new String(guid.ToString().Where(Char.IsDigit).ToArray());
+                var password = int.Parse(justNumbers.Substring(4, 4));
+                clienteR.PASSWORD_CLIENTE = password.ToString();
+
+
+                bd.CLIENTE.Add(clienteR);
+                bd.SaveChanges();
+                resul.mensaje = "<i class='zmdi zmdi-check zmdi-hc-fw'></i>Cliente Registrado Correctamente</br>";
+                resul.mensaje = resul.mensaje + "<i class='zmdi zmdi-account-add zmdi-hc-fw'></i>Usuario: " + username + "</br>";
+                resul.mensaje = resul.mensaje + "<i class='zmdi zmdi-account-add zmdi-hc-fw'></i>Password: " + password ;
+            }
+            else
+            {
+                resul.mensaje = "<b>Error</b></br>" + resul.mensaje;
+            }
+                return Json(resul);
+        }
+            
+        [HttpPost]
+        [Authorize]
+        public JsonResult EliminarC(int id)
+        {
+            EntitiesNoMasAccidentes bd = new EntitiesNoMasAccidentes();
+            var resul = new baseRespuesta();
+            var clienteR = bd.CLIENTE.Find(id);
+            clienteR.ACTIVO_CLIENTE = "N";
+            bd.Entry(clienteR).State = System.Data.EntityState.Modified;
             bd.SaveChanges();
-            return Json("d");
+            resul.mensaje = "Cliente eliminado correctamente";
+            return Json(resul);
+        }
 
-
-
+        public class baseRespuesta
+        {
+            public bool ok { get; set; }
+            public string mensaje { get; set; }
         }
     }
 }
