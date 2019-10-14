1508472df440ff06f7c8179e135d0f19f3920c08
Ro <robertopizarro199@gmail.com>
2019-10-14 17:25:45 -0600

    editarv1

--- NoMasAccidentes/ViewModels/IndexViewModel.cs
+++ NoMasAccidentes/ViewModels/IndexViewModel.cs
@@ -16,7 +16,7 @@ namespace NoMasAccidentes.ViewModels
 
         public List<Models.RUBRO> rubro { get; set; }
 
-        public List<Models.RUBRO> rubro { get; set; }
+       
     }
 
     //public class RubroViewModel : BaseModelo
